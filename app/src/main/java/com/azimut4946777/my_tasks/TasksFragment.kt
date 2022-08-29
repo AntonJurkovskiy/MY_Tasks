7e28512460668552fc9dfd16c9b8c2602ade26d7
AntonJurkovskiy <azimut4946777@gmail.com>
2022-08-29 10:36:04 +0300

    added DataBase

--- app/src/main/java/com/azimut4946777/my_tasks/TasksFragment.kt
+++ app/src/main/java/com/azimut4946777/my_tasks/TasksFragment.kt
@@ -5,25 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.azimut4946777.my_tasks.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment() {
     private var _binding: FragmentTasksBinding? = null
-private val binding get() = _binding!!
+    private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-_binding = FragmentTasksBinding.inflate(inflater,container,false)
-val view = binding.root
-
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
+        val viewModelFactory = TasksViewModelFactory(dao)
+        val viewModel = ViewModelProvider(
+            this, viewModelFactory).get(TasksViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         return view
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
