b255e85289435f04e93562bd526a942ee31934ec
AntonJurkovskiy <azimut4946777@gmail.com>
2022-08-31 11:51:27 +0300

    added EditFragment and navigation graph. fixed bug with resource file access

--- app/src/main/java/com/azimut4946777/my_tasks/TasksViewModel.kt
+++ app/src/main/java/com/azimut4946777/my_tasks/TasksViewModel.kt
@@ -1,15 +1,16 @@
 package com.azimut4946777.my_tasks
 
 import android.view.View
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.launch
 
 class TasksViewModel(val dao: TaskDao) : ViewModel() {
 
     var newTaskName = ""
     val tasks = dao.getAll()
+private val _navigateToTask = MutableLiveData<Long?>()
+    val  navigateToTask: LiveData<Long?>
+    get() = _navigateToTask
 
     fun addTask() {
         viewModelScope.launch {
@@ -18,6 +19,14 @@ class TasksViewModel(val dao: TaskDao) : ViewModel() {
             dao.insert(task)
 
         }
+    }
+
+    fun onTaskClicked(taskId: Long) {
+        _navigateToTask.value = taskId
+    }
+
+    fun onTaskNavigated() {
+        _navigateToTask.value = null
     }
 
 }
